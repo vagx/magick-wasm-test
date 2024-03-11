5e8418720d3419b8e4ddcd4f60a04c9559f7a502
chergav <gav3253975@gmail.com>
2024-03-11 08:11:00 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
 	base: '/magick-wasm-test/',
+	build: {
+		target: 'esnext'
+	},
 	resolve: {
 		alias: { '~': fileURLToPath(new URL('./src', import.meta.url)) }
 	},
